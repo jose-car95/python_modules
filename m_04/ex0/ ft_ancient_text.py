c3d78368f710a76007c166e2808d63fd7ce83ce9
Jose <josecgb95@gmail.com>
2026-03-23 16:14:28 +0100

    begining module_04

---
+++ m_04/ex0/ ft_ancient_text.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def main() -> None:
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    try:
+        print("Accessing Storage Vault: ancient_fragment.txt")
+        with open("ancient_fragment.txt", "r") as file:
+            print("Connection established...")
+            data: list[str] = [line.rstrip("\n") for line in file]
+        print("RECOVERED DATA:")
+        for fragment in data:
+            print(fragment)
+        print("\nData recovery complete. Storage unit disconnected.")
+
+    except FileNotFoundError:
+        print("ERROR: Storage vault not found. Run data generator first.")
+
+
+if __name__ == "__main__":
+    main()
