48eeec885a20c1b36b282fb6ee7131679df422be
Jose Carlos <josecgb95@gmail.com>
2026-03-30 13:30:27 +0200

    change the updates

--- m_02/ex0/ft_first_exception.py
+++ m_02/ex0/ft_first_exception.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-def check_temperature(temp_str: str) -> int | None:
+def input_temperature(temp_str: str) -> int | None:
     try:
         temp: int = int(temp_str)
 
@@ -21,19 +21,19 @@ def check_temperature(temp_str: str) -> int | None:
 
 
 def test_temperature_input() -> None:
-    print("=== Garden Temperature Checker ===\n")
+    print("=== Garden Temperature ===\n")
 
     print("Testing temperature: 25")
-    check_temperature("25")
+    input_temperature("25")
     print()
     print("Testing temperature: abc")
-    check_temperature("abc")
+    input_temperature("abc")
     print()
     print("Testing temperature: 100")
-    check_temperature("100")
+    input_temperature("100")
     print()
     print("Testing temperature: -50")
-    check_temperature("-50")
+    input_temperature("-50")
 
     print("\nAll tests completed - program didn't crach!")
 
